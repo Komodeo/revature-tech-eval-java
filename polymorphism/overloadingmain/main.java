be334149083e1dbdbb79806503a929519ce771ca
Grant Brown <onlythebest@gmail.com>
2024-05-15 17:21:48 -0700

    method overriding
    and overloading main

---
+++ polymorphism/overloadingmain/main.java
@@ -0,0 +1,17 @@
+// Overloading the main() method in Java
+
+package polymorphism.overloadingmain;
+
+class TestOverloading4 {
+    public static void main(String[] args) {
+        System.out.println("main with String[]");
+    }
+
+    public static void main(String args) {
+        System.out.println("main with String");
+    }
+
+    public static void main() {
+        System.out.println("main without args");
+    }
+}
