86eccb2fad7fbd8fecd8670cce218724ae7233b3
Grant Brown <onlythebest@gmail.com>
2024-05-16 10:43:00 -0700

    read and write only encapsulation

---
+++ encapsulation/readonly/Student.java
@@ -0,0 +1,13 @@
+//A Java class which has only getter methods.  
+
+package encapsulation.readonly;
+
+public class Student {
+    // private data member
+    private String college = "AKG";
+
+    // getter method for college
+    public String getCollege() {
+        return college;
+    }
+}
---
+++ encapsulation/writeonly/Student.java
@@ -0,0 +1,13 @@
+//A Java class which has only setter methods.  
+
+package encapsulation.writeonly;
+
+public class Student {
+    // private data member
+    private String college;
+
+    // getter method for college
+    public void setCollege(String college) {
+        this.college = college;
+    }
+}
