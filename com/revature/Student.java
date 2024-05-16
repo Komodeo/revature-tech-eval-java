181cb9dc49f87ad90066c3af6780114422d85c98
Grant Brown <onlythebest@gmail.com>
2024-05-16 10:34:59 -0700

    encapsulation example

---
+++ com/revature/Student.java
@@ -0,0 +1,17 @@
+// Example for Encapsulation - Student
+package com.revature;
+
+public class Student {
+    // private data member
+    private String name;
+
+    // getter method for name
+    public String getName() {
+        return name;
+    }
+
+    // setter method for name
+    public void setName(String name) {
+        this.name = name;
+    }
+}
