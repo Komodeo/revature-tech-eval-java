096e687ec3231875051820d7560b84929536223f
Grant Brown <onlythebest@gmail.com>
2024-05-15 17:05:05 -0700

    method overloading by type

---
+++ polymorphism/overloadingnumber/Adder.java
@@ -0,0 +1,20 @@
+// Method Overloading  - Changing the number of arguments
+
+package polymorphism.overloadingnumber;
+
+public class Adder {
+    static int add(int a, int b) {
+        return a + b;
+    } // 2 arguments
+
+    static int add(int a, int b, int c) {
+        return a + b + c;
+    } // 3 arguments
+}
+
+class TestOverloading1 {
+    public static void main(String[] args) {
+        System.out.println(Adder.add(11, 11));
+        System.out.println(Adder.add(11, 11, 11));
+    }
+}
