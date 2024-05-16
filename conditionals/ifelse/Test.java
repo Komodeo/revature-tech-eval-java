9a40be6807013136ed34dfa0c37df7adea1be561
Grant Brown <onlythebest@gmail.com>
2024-05-16 12:19:38 -0700

    if else example

---
+++ conditionals/ifelse/Test.java
@@ -0,0 +1,14 @@
+package conditionals.ifelse;
+
+public class Test {
+
+    public static void main(String args[]) {
+       int x = 30;
+ 
+       if( x < 20 ) {
+          System.out.print("This is if statement");
+       }else {
+          System.out.print("This is else statement");
+       }
+    }
+ }
