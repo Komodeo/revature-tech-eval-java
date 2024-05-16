35736e6efdeb90a1200009f50b6df72125589010
Grant Brown <onlythebest@gmail.com>
2024-05-16 11:58:29 -0700

    abstraction sample begin

---
+++ abstraction/sampleexercise/Bank.java
@@ -0,0 +1,23 @@
+/* Sample Exercise : 
+To create an abstract class called Bank.
+To create an abstract method called getRateOfInterest();
+To create two subclasses called SBI - 7% and PNB - 5% as two banks that extend the abstract class Bank.
+To implement different functionalities for the getRateOfInterest() method in the SBI and PNB classes through the concept of method overriding and print out the interest rate inside the main() method created separately in a test class called TestBank */
+
+package abstraction.sampleexercise;
+
+abstract class Bank { // Abstract class
+    abstract float getRateOfInterest(); // Abstract method
+}
+
+class SBI extends Bank {
+    float getRateOfInterest() {
+        return 7f;
+    }
+}
+
+class PNB extends Bank {
+    float getRateOfInterest() {
+        return 5f;
+    }
+}
