1da3da5ef7c16693555f155d324d9b0326c9c57e
Grant Brown <onlythebest@gmail.com>
2024-05-16 12:18:14 -0700

    conditional example

---
+++ conditionals/example/Test.java
@@ -0,0 +1,14 @@
+// Example conditional statement
+
+package conditionals.example;
+
+public class Test {
+
+    public static void main(String args[]) {
+       int x = 10;
+ 
+       if( x < 20 ) {
+          System.out.print("This is if statement");
+       }
+    }
+ }
