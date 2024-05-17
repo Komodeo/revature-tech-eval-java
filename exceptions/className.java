95e8719315b58554bcf2453664ea85ee7cfe585e
Grant Brown <onlythebest@gmail.com>
2024-05-16 21:50:34 -0700

    throw throws

---
+++ exceptions/className.java
@@ -0,0 +1,14 @@
+// Example throw/throws keywords
+
+package exceptions;
+
+import java.rmi.RemoteException;
+
+public class className {
+
+    public void deposit(double amount) throws RemoteException {
+        // Method implementation
+        throw new RemoteException();
+    }
+    // Remainder of class definition
+}
