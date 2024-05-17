e48cc3034f25d8f62d2498e2b56876f9d9c71595
Grant Brown <onlythebest@gmail.com>
2024-05-16 20:49:28 -0700

    strings

---
+++ strings/stringcreation/StringDemo.java
@@ -0,0 +1,12 @@
+// Example creating strings
+
+package strings.stringcreation;
+
+public class StringDemo {
+
+    public static void main(String args[]) {
+        char[] helloArray = { 'h', 'e', 'l', 'l', 'o', '.' };
+        String helloString = new String(helloArray);
+        System.out.println(helloString);
+    }
+}
---
+++ strings/stringlength/StringDemo.java
@@ -0,0 +1,12 @@
+// Example string length
+
+package strings.stringlength;
+
+public class StringDemo {
+
+    public static void main(String args[]) {
+        String palindrome = "Dot saw I was Tod";
+        int len = palindrome.length(); // Using the length method
+        System.out.println("String Length is : " + len);
+    }
+}
