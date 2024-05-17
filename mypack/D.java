fd8a84d6e1899d5329b9b9ae5d9279152519147d
Grant Brown <onlythebest@gmail.com>
2024-05-16 21:12:15 -0700

    fully qualified name

---
+++ mypack/D.java
@@ -0,0 +1,9 @@
+//save by D.java  
+package mypack;
+
+class D {
+    public static void main(String args[]) {
+        pack.A obj = new pack.A(); // using fully qualified name
+        obj.msg();
+    }
+}
