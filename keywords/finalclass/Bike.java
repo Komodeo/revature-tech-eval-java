cf8781677dddb7a40d231dee5ed1d5bdfc3ec980
Grant Brown <onlythebest@gmail.com>
2024-05-16 20:18:43 -0700

    final class

---
+++ keywords/finalclass/Bike.java
@@ -0,0 +1,17 @@
+// Example final class
+
+package keywords.finalclass;
+
+final class Bike {
+}
+
+// class Honda1 extends Bike { // Compile Time Error (Cant extend final class)
+//     void run() {
+//         System.out.println("running safely with 100kmph");
+//     }
+
+//     public static void main(String args[]) {
+//         Honda1 honda = new Honda1();
+//         honda.run();
+//     }
+// }
