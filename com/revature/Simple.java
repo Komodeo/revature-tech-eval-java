6601ae3511b06a665de382f03489a4372e4a32f6
Grant Brown <onlythebest@gmail.com>
2024-05-16 21:04:13 -0700

    package example

---
+++ com/revature/Simple.java
@@ -0,0 +1,10 @@
+// Package example
+
+//save as Simple.java  
+package com.revature;
+
+public class Simple {
+    public static void main(String args[]) {
+        System.out.println("Welcome to package");
+    }
+}
